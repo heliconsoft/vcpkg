de8851c667624ecbc9d023f67d9409844661ce18
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2022-10-25 23:36:29 +0300

    xmp-sdk-config.cmake

--- ports/xmp-sdk/xmp-sdk-config.cmake
+++ ports/xmp-sdk/xmp-sdk-config.cmake
@@ -3,13 +3,24 @@ get_filename_component(ROOT "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(ROOT "${ROOT}" PATH)
 get_filename_component(ROOT "${ROOT}" PATH)
 
-add_library(xmp-sdk::xmp-sdk STATIC IMPORTED)
-set_target_properties(xmp-sdk::xmp-sdk PROPERTIES
+add_library(xmp-sdk::xmp-sdk-core STATIC IMPORTED)
+set_target_properties(xmp-sdk::xmp-sdk-core PROPERTIES
+    IMPORTED_LOCATION_DEBUG "${ROOT}/debug/lib/XMPCoreStatic.lib"
+    IMPORTED_LOCATION "${ROOT}/lib/XMPCoreStatic.lib"
 	IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
-	IMPORTED_LOCATION_RELEASE "${ROOT}/lib/XMPCoreStatic.lib"
-	IMPORTED_LOCATION_DEBUG "${ROOT}/debug/lib/XMPCoreStatic.lib"
 	INTERFACE_INCLUDE_DIRECTORIES "${ROOT}/include/xmp"
 )
+
+add_library(xmp-sdk::xmp-sdk-files STATIC IMPORTED)
+set_target_properties(xmp-sdk::xmp-sdk-files PROPERTIES
+    IMPORTED_LOCATION_DEBUG "${ROOT}/debug/lib/XMPFilesStatic.lib"
+    IMPORTED_LOCATION "${ROOT}/lib/XMPFilesStatic.lib"
+    IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+    INTERFACE_INCLUDE_DIRECTORIES "${ROOT}/include/xmp"
+)
+
+add_library(xmp-sdk::xmp-sdk INTERFACE IMPORTED)
+target_link_libraries(xmp-sdk::xmp-sdk INTERFACE xmp-sdk::xmp-sdk-core xmp-sdk::xmp-sdk-files)
 
 #When target dependencies are specified using target_link_libraries(), CMake will read this property from all target dependencies to determine the build properties of the consumer. 
 target_compile_definitions(xmp-sdk::xmp-sdk INTERFACE WIN_ENV)
