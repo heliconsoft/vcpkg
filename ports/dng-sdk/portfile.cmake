1600c32e14ba52d82afe775b78854842dff1ce32
cos <5281067+cos-public@users.noreply.github.com>
2022-10-28 14:47:42 +0100

    dng-sdk seems building. -config.cmake file WIP

--- ports/dng-sdk/portfile.cmake
+++ ports/dng-sdk/portfile.cmake
@@ -13,7 +13,10 @@ vcpkg_extract_source_archive(
 file(REMOVE_RECURSE "${SOURCE_PATH}/libjpeg/" "${SOURCE_PATH}/xmp/")
 file(REMOVE "${SOURCE_PATH}/JPEG_ReadMe.txt" "${SOURCE_PATH}/XMP_ReadMe.txt")
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY
+	"${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"
+	"${CMAKE_CURRENT_LIST_DIR}/config.cmake.in"
+	DESTINATION "${SOURCE_PATH}")
 
 #TODO: check and pass optional features
 #if("jpeg" IN_LIST FEATURES)
@@ -24,8 +27,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-#file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "The latest license is available at https://www.adobe.com/support/downloads/dng/dng_sdk_eula_win.html")
