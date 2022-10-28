b6a25a440ef43fb36ae8eca0516d4b711510715a
cos <5281067+cos-public@users.noreply.github.com>
2022-10-28 19:49:17 +0100

    config.cmake files generated (usage not tested)

--- ports/dng-sdk/portfile.cmake
+++ ports/dng-sdk/portfile.cmake
@@ -27,9 +27,8 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
+vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
